12dd125f6e70812f5fa640aa67245b15713b0de0
tomon9086 <tomo.mouse.9086@gmail.com>
2019-05-18 15:34:57 +0900

    create components

---
+++ src/views/Catalog.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="catalog">
+    <h1>This is an catalog page</h1>
+  </div>
+</template>
---
+++ src/views/Editor.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="editor">
+    <h1>This is an editor page</h1>
+  </div>
+</template>
