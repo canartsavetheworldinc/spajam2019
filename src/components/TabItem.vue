914c80018b96e907ab0e4c5bb5370d4c4d70a3d6
tomon9086 <tomo.mouse.9086@gmail.com>
2019-05-19 08:12:00 +0900

    tab

--- src/components/TabItem.vue
+++ src/components/TabItem.vue
@@ -1,17 +1,26 @@
 <template>
-  <div>
+  <div class="tab-item-container">
+    <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'TabItem',
-  props: {
-    msg: String
-  }
+  name: 'TabItem'
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.tab-item-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  min-width: 100px;
+  width: 100px;
+  height: 30px;
+  margin: 0 5px;
+  padding-bottom: 4px;
+  /* border: 1px solid black; */
+}
 </style>
--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="tabs-container">
-    tabs
+    <div class="tab">
+      <TabItem :key="i" v-for="(tab, i) in tabs">{{ tab }}</TabItem>
+    </div>
+    <div class="neon"></div>
   </div>
 </template>
 
 <script>
+import TabItem from '@/components/TabItem'
+
 export default {
   name: 'Tabs',
   props: {
     msg: String
+  },
+  components: {
+    TabItem
+  },
+  data () {
+    return {
+      tabs: ['HOT', 'IT', 'ビジネス', 'スポーツ']
+    }
   }
 }
 </script>
@@ -17,8 +30,20 @@ export default {
 <style scoped>
 .tabs-container {
   display: flex;
-  align-items: flex-end;
+  flex-direction: column;
+  justify-content: flex-end;
   height: 50px;
   background-color: #9cfaa0;
+}
+.tab {
+  display: flex;
+  align-items: flex-end;
+  height: 100%;
+  overflow-x: scroll;
+}
+.neon {
+  width: 100%;
+  height: 3px;
+  background-color: #ff14c0;
 }
 </style>
