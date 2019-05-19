f3ae695a8a271cc386e594404b2febb01a2f3224
tomon9086 <tomo.mouse.9086@gmail.com>
2019-05-19 10:31:46 +0900

    detail navigator

---
+++ src/components/DetailHeader.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="detail-header-container">
+    <div class="navigator back" @click="back">Back</div>
+    <!-- <div class="logo-container"></div> -->
+    <div class="navigator bookmark" @click="bookmark">Bookmark</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailHeader',
+  props: {
+    msg: String
+  },
+  methods: {
+    back () {
+      this.$router.go(-1)
+    },
+    bookmark () {
+      console.log('bookmarked!')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.detail-header-container {
+  display: flex;
+  justify-content: space-between;
+  background-color: #3D3D3D;
+  background-image: url("/img/icons/apple-touch-icon-60x60.png");
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+}
+.navigator {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 112px;
+  height: 100%;
+}
+.navigator.back {
+  background-color: #9CFAA0;
+}
+.navigator.bookmark {
+  background-color: #FEA3E6;
+}
+/* .logo-container {
+} */
+</style>
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="detail">
-    <h1>{{ article.title }}</h1>
-    <p>{{ article.content }}</p>
+  <div class="detail-container">
+    <DetailHeader class="header"></DetailHeader>
+    <div class="content">
+      <h1>{{ article.title }}</h1>
+      <p>{{ article.content }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import DetailHeader from '@/components/DetailHeader'
+
 export default {
+  components: {
+    DetailHeader
+  },
   computed: {
-    debug () {
-      console.log(this.$route.params.id)
-      return 'debug'
-    },
     article () {
       const id = this.$route.params.id
       if (id === undefined) {
@@ -22,3 +26,18 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.header {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  /* width: 100vw; */
+  min-height: 50px;
+  height: 50px;
+}
+.content {
+  margin-top: 70px;
+}
+</style>
