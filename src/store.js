0adb1e608622371caae521a77ac986cef3d007e0
tomon9086 <tomo.mouse.9086@gmail.com>
2019-05-19 13:40:50 +0900

    サンプルデータ消去

--- src/store.js
+++ src/store.js
@@ -13,36 +13,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    articles: [
-      {
-        title: 'あっほー',
-        content: 'hoge'
-      },
-      {
-        title: 'いっほー',
-        content: 'hogehoge'
-      },
-      {
-        title: 'うっほー',
-        content: 'fuga'
-      },
-      {
-        title: 'えっほー',
-        content: 'fugafuga'
-      },
-      {
-        title: 'おっほー',
-        content: 'hogefuga'
-      },
-      {
-        title: 'かっほー',
-        content: 'hogefuga'
-      },
-      {
-        title: 'きっほー',
-        content: 'hogefuga'
-      }
-    ]
+    articles: []
   },
   mutations: {
     update_articles (context) {
